f0fe35e3a7da67efa4f2b54a8a5d8e10a82a399d
Gergan Nikolov <gergan.n@logicpromot.com>
2017-04-26 10:58:05 +0300

    Homepage connected with ACF

--- views/banner.php
+++ views/banner.php
@@ -2,23 +2,24 @@
 /*
 *   View: Homepage banner
 */
+
+$page_id = get_the_ID();
+$slider_elements = get_field( "slider_posts", $page_id );
 ?>
 <div class="banner">
     <div class="owl-carousel owl-theme owl-loaded">
-        <div class="owl-item" style="background-image: url(https://images.unsplash.com/photo-1483647195573-d3128d9b67be);">
+        <?php
+        foreach ( $slider_elements as $element_ ) {
+            $post_title = $element_->post_title;
+            $post_url = get_permalink( $element_->ID );
+            $post_featured_image = get_the_post_thumbnail_url( $element_->ID, "full" );
+        ?>
+        <div class="owl-item" style="background-image: url(<?php echo $post_featured_image; ?>);">
             <div class="content">
-                <h1 class="header">Hello from Reactive!</h1>
+                <h1 class="header"><?php echo $post_title; ?></h1>
+                <a href="<?php echo $post_url; ?>" class="post-anchor">Read it!</a>
             </div>
         </div>
-        <div class="owl-item" style="background-image: url(https://images.unsplash.com/photo-1483647195573-d3128d9b67be);">
-            <div class="content">
-                <h1 class="header">Second slide</h1>
-            </div>
-        </div>
-        <div class="owl-item" style="background-image: url(https://images.unsplash.com/photo-1483647195573-d3128d9b67be);">
-            <div class="content">
-                <h1 class="header">Third slide</h1>
-            </div>
-        </div>
+        <?php } ?>
     </div>
 </div>
--- views/homepage-content.php
+++ views/homepage-content.php
@@ -2,65 +2,72 @@
 /*
 *   View: Homepage content
 */
+
+$page_id = get_the_ID();
+$featured_posts = get_field( "featured_posts", $page_id );
+$featured_post = get_field( "featured_post", $page_id )[0];
+$featured_category = get_field( "featured_category", $page_id );
 ?>
 <div class="homepage-content">
     <div id="featured-posts" class="small-boxes">
-        <a href="#!" class="post-anchor">
-            <div class="box">
-                <h1 class="box-header">Lorem Ipsum</h1>
-                <div class="content">
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.
-                </div>
+        <?php
+        foreach ( $featured_posts as $post_ ) {
+            $post_url = get_permalink( $post_->ID );
+            $post_title = $post_->post_title;
+            $post_excerpt = wp_trim_words( $post_->post_content, "20", "..." );
+            $post_featured_image = get_the_post_thumbnail_url( $post_->ID, "full" );
+        ?>
+        <a href="<?php echo $post_url; ?>" class="post-anchor">
+            <div class="box" style="background-image: url(<?php echo $post_featured_image; ?>);">
+                <h1 class="box-header"><?php echo $post_title; ?></h1>
+                <div class="content"><?php echo $post_excerpt; ?></div>
             </div>
         </a>
-        <a href="#!" class="post-anchor">
-            <div class="box">
-                <h1 class="box-header">Hey, hi, hello!</h1>
-            </div>
-        </a>
-        <a href="#!" class="post-anchor">
-            <div class="box">
-                <h1 class="box-header">Stay active, follow Reactive!</h1>
-                <div class="content">
-                    Looking for a coffee partner?<br>Follow Reactive and stay active trough the whole day, week or even month!
-                </div>
-            </div>
-        </a>
+        <?php } ?>
     </div>
     <div id="last-post" class="full-width-box">
+        <?php
+        $featured_post_url = get_permalink( $featured_post->ID );
+        $featured_post_excerpt = wp_trim_words( $featured_post->post_content, "35", "..." );
+        $featured_post_featured_image = get_the_post_thumbnail_url( $featured_post->ID, "full" );
+        ?>
         <div class="content">
-            <h1 class="header">Latest post!</h1>
+            <h1 class="header"><?php echo $featured_post->post_title; ?></h1>
             <div class="text">
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                <p><?php echo $featured_post_excerpt; ?></p>
             </div>
-            <a href="#!" class="read-more">Read it!</a>
+            <a href="<?php echo $featured_post_url; ?>" class="read-more">Read it!</a>
         </div>
-        <div class="featured-image" style="background-image: url(https://images.unsplash.com/photo-1483058712412-4245e9b90334);">
+        <div class="featured-image" style="background-image: url(<?php echo $featured_post_featured_image; ?>);">
         </div>
     </div>
     <div id="featured-category" class="featured-category">
-        <h1 class="header">Featured Category:</h1>
+        <?php
+        $featured_category_name = get_cat_name( $featured_category );
+
+        $args = array(
+            "posts_per_page" => 4,
+            "post_type" => "post",
+            "post_status" => "publish",
+            "orderby" => "ID",
+            "order" => "DESC",
+            "category" => $featured_category
+        );
+        $posts_ = get_posts( $args );
+        ?>
+        <h1 class="header"><?php echo $featured_category_name; ?>:</h1>
         <div class="tiny-boxes">
-            <a href="#!" class="post-anchor">
+            <?php
+            foreach ( $posts_ as $post_ ) {
+                $post_url = get_permalink( $post_->ID );
+                $post_featured_image = get_the_post_thumbnail_url( $post_->ID, "full" );
+            ?>
+            <a href="<?php echo $post_url; ?>" class="post-anchor" style="background-image: url(<?php echo $post_featured_image; ?>);">
                 <div class="box">
-                    <h1 class="box-header">Lorem Ipsum</h1>
+                    <h1 class="box-header"><?php echo $post_->post_title; ?></h1>
                 </div>
             </a>
-            <a href="#!" class="post-anchor">
-                <div class="box">
-                    <h1 class="box-header">Lorem Ipsum</h1>
-                </div>
-            </a>
-            <a href="#!" class="post-anchor">
-                <div class="box">
-                    <h1 class="box-header">Lorem Ipsum</h1>
-                </div>
-            </a>
-            <a href="#!" class="post-anchor">
-                <div class="box">
-                    <h1 class="box-header">Lorem Ipsum</h1>
-                </div>
-            </a>
+            <?php } ?>            
         </div>
     </div>
 </div>
